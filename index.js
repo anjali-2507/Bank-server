849fc404699d53ef27f1edf5f7018f07a9fa4f1b
anjali-2507 <anjalivarma200107@gmai.com>
2025-03-12 23:27:08 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://bank-client-one.vercel.app/login',
+    origin: 'https://bank-client-one.vercel.app',
 }));
 app.use(express.json());
 
